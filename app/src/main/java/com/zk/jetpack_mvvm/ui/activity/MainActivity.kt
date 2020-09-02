52c6655b843406c608e5a7f3fed2f33f9bd0a9af
zkdengta <1015422160@qq.com>
2020-09-02 22:09:48 +0800

    集成jetpackMvvm基础架构

--- app/src/main/java/com/zk/jetpack_mvvm/ui/activity/MainActivity.kt
+++ app/src/main/java/com/zk/jetpack_mvvm/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.zk.jetpack_mvvm
+package com.zk.jetpack_mvvm.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zk.jetpack_mvvm.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
